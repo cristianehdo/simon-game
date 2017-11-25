f1f731b4b8946520a781dd68bc9eafe88e96dcb7
Cristiane Silva <cristiane.hdo@gmail.com>
2017-11-25 15:11:50 +0000

    fetching user clicks

---
+++ js/game.js
@@ -0,0 +1,10 @@
+// easy model 4 possibilities
+
+const randomEasy = () => {
+// random number from 1 to 4
+  return Math.floor(Math.random() * 4) + 1;
+}
+
+const verifySequence = (number) => {
+  console.log(number);
+}
---
+++ js/user-action.js
@@ -0,0 +1,22 @@
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const cards = document.querySelectorAll(".card");
+const numbers = ["one", "two", "three", "four"];
+
+
+
+const getSelectedCard = (card) => {
+  numbers.forEach((number) => {
+    if (card.classList.contains(number)) {
+       verifySequence(number);
+    }
+  });
+};
+
+cards.forEach((card) => {
+  card.addEventListener("click", (event) => {
+    getSelectedCard(event.currentTarget);
+  });
+});
